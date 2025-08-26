64600d04d760ca02ec3ebd0125d26fed96dc84fe
Palak Kumar <Palak.kumar@somaiya.edu>
2025-08-26 23:44:33 +0530

    Add files via upload

---
+++ A.Next Round.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int a[55];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int cutoff = a[k-1];
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] >= cutoff && a[i] > 0) {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
